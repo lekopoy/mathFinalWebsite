1e4e59e63d728ae0a63af90e0b43a5cfd42dce67
Dylan Lekopoy <lekopoyd@gmail.com>
2026-06-14 23:02:04 -0600

    finished

--- scripts/slideshow/main.js
+++ scripts/slideshow/main.js
@@ -2,104 +2,125 @@ const sequence = [
   {
     name: "cb-decimal",
     aniType: "animation-one",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "cb-hexadecimal",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "cb-binary",
     aniType: "animation-two",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "binary-header",
     aniType: "animation-three",
-    sectionChange: 640
+    sectionChange: true
   },
   {
     name: "binary",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "binary-2",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "binary-3",
     aniType: "animation-one",
-    sectionChange: 1400
+    sectionChange: true
   },
   {
     name: "bi3-1",
     aniType: "animation-one",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "hexa-header",
     aniType: "animation-three",
-    sectionChange: 2400
+    sectionChange: true
   },
   {
     name: "hexa",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "hexa-2",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "decimal-header",
     aniType: "animation-three",
-    sectionChange: 3500
+    sectionChange: true
   },
   {
     name: "decimal",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "decimal-2",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "citation-header",
     aniType: "animation-three",
-    sectionChange: 4600
+    sectionChange: true
   },
   {
     name: "citation",
     aniType: "animation-one",
-    sectionChange: 0
+    sectionChange: false
   },
   {
     name: "end",
     aniType: "animation-three",
-    sectionChange: 0
+    sectionChange: false
   }
 ]
 
-let currentAnimation = sequence[0];
-
-window.addEventListener('keydown', (event) => {
-  if (currentAnimation.name === "end" || event.key !== "ArrowRight" && event.key !== "e" && event.key !== "Enter") return;
-
+function arrowR() {
   const elementToAnimate = document.querySelector(`.${currentAnimation.name}`);
 
-  if (currentAnimation.sectionChange !== 0) {
-    window.scrollTo({
-      top: currentAnimation.sectionChange,
-      behavior: 'smooth'
+  if (currentAnimation.sectionChange === true) {
+    elementToAnimate.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
     });
   }
 
   elementToAnimate.classList.add(currentAnimation.aniType);
   currentAnimation = sequence[sequence.indexOf(currentAnimation) + 1];
+}
+
+function arrowL(secondAnimation) {
+  secondAnimation = sequence[sequence.indexOf(secondAnimation) - 1];
+  const elementToAnimate = document.querySelector(`.${secondAnimation.name}`);
+
+  elementToAnimate.scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+
+  return secondAnimation;
+}
+
+let currentAnimation = sequence[0];
+let secondAnimation = sequence[0];
+
+window.addEventListener('keydown', (event) => {
+  if (currentAnimation.name === "end" || event.key !== "ArrowRight" && event.key !== "e" && event.key !== "Enter" && event.key !== "ArrowLeft") return;
+
+  if (event.key === "ArrowRight" || event.key === "e" || event.key === "Enter") {
+    currentAnimation, secondAnimation = arrowR();
+  } else if (event.key === "ArrowLeft") {
+    secondAnimation = arrowL(secondAnimation);
+  };
 });
